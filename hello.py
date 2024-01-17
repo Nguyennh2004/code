c824d0ac0a6f14482ecdaa846491f4aa65a0fc9b
Nguyennh2004 <nguyenskt2004@gmail.com>
2024-01-17 19:42:38 +0700

    dua ma nguon len github

---
+++ hello.py
@@ -0,0 +1,48 @@
+import turtle 
+#t.forward(x) - tiến tới
+#t.right(x) - quay phải 
+#t.goto(x,y) - (di chuyển dến)
+#t.pendown() - đặt bút xuống
+#t.circle(x) - vẽ hình tròn
+t = turtle.Turtle()
+
+t.color('red')
+t.fillcolor('black')
+
+t.dot(20)
+
+t.penup()
+t.goto(-200,-200)
+t.pendown()
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+t.right(45)
+t.forward(80)
+
+t.right(97)
+t.forward(73)
+
+t.penup()
+t.goto (200,200)
+t.pendown()
+t.circle(50)
+
+t.penup()
+t.goto(100,100)
+t.pendown()
+
+
+
+
+
+turtle.done()
+
+
+turtle.done()
+
