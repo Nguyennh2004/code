05d63dbf510d86763aa8fac2ba1f619edaa09a10
Nguyennh2004 <nguyenskt2004@gmail.com>
2024-01-17 21:24:51 +0700

    03- Bien toan tu va kieu du lieu

---
+++ bien.py
@@ -0,0 +1,4 @@
+ten_nhom = "C0124L1"
+so_thanh_vien = 20
+da_co_gia_dinh_chua = False
+tong_so_can_nang = 2207
---
+++ sum.py
@@ -0,0 +1,9 @@
+# input a,b
+a = 5
+b = 7
+
+# xu li 
+c = a + b
+
+# output<xuat>
+print( c )
